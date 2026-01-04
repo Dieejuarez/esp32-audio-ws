2ab57d6a9fdd9fe520fb48e7b52ac3231c95cc8c
Dieejuarez <79687724+Dieejuarez@users.noreply.github.com>
2026-01-04 00:58:16 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 import asyncio
 import websockets
+import pyaudio
 
-async def audio_server(ws):
+RATE = 48000
+CHANNELS = 1
+FORMAT = pyaudio.paInt16
+
+p = pyaudio.PyAudio()
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    output=True
+)
+
+async def handler(ws):
     print("ESP32 conectada")
     async for data in ws:
-        print("Bytes:", len(data))
+        stream.write(data)
 
 async def main():
-    async with websockets.serve(audio_server, "0.0.0.0", 8080):
+    async with websockets.serve(handler, "0.0.0.0", 8080):
+        print("Servidor WebSocket listo")
         await asyncio.Future()
 
 asyncio.run(main())
